5e860aff4eced3301139c662efe1a953ce920100
bertrand <714356388@qq.com>
2016-05-19 15:06:58 +0800

    add charts

---
+++ resources/views/charts/lineChart.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('content')
+
+<div style="width : 500px; height:600px;">
+<canvas id="graph"></canvas>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.3/Chart.min.js"> </script>
+<script>
+    window.onload = function () {
+        var data={
+            labels : ['1', '2', '3'],
+            datasets: [
+                {
+                    label: "fuck you",
+                    backgroundColor:"rgba(100, 220, 220, 0.2)",
+                    data : [30, 20, 100],
+                },
+                {
+                    label: "fuck me",
+                    data : [30, 200, 100],
+                }
+            ]
+        };
+        var context =document.querySelector('#graph').getContext('2d');
+        new Chart.Line(context, {"data": data});
+    }
+</script>
+@endsection
